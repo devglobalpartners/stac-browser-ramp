5c5990102fb1e026fef48f507f1eae4cd7cf14d7
Matthias Mohr <m.mohr@uni-muenster.de>
2022-08-31 00:04:06 +0200

    Don't show collections for source imagery

--- config.js
+++ config.js
@@ -30,7 +30,7 @@ module.exports = {
     crossOriginMedia: null,
     requestHeaders: {},
     requestQueryParameters: {},
-    collectionsFilter: c => c.id.startsWith("ramp_"),
+    collectionsFilter: c => c.id.startsWith("ramp_") && !c.id.endsWith('_source'),
     preprocessSTAC: preprocess,
     authConfig: {
         type: 'query', // null or 'query' or 'header'
